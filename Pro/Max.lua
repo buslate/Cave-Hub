efc1e1083564c0eb99091bf0eb3fbf78c8a93a33
Cave Hub <113223653+buslate@users.noreply.github.com>
2024-05-06 20:23:11 +0700

    Update Max.lua

--- Pro/Max.lua
+++ Pro/Max.lua
@@ -1,6 +1,7 @@
 local placeId = game.PlaceId
+_G.Kaitun = "BuildABoatForTreasure"
 
-if _G.Kaitun = "BuildABoatForTreasure" then
+if _G.Kaitun == "BuildABoatForTreasure" then
     loadstring(game:HttpGet(("https://raw.githubusercontent.com/buslate/Kaitun-BAFT/main/kai/na/hee/kuy.lua"), true))()
 else
     if placeId == 537413528 then
