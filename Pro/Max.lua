3dfba52be6c7cf844efc022f35c6cf71ff55a00d
Cave Hub <113223653+buslate@users.noreply.github.com>
2024-05-18 12:53:40 +0700

    Update Max.lua

--- Pro/Max.lua
+++ Pro/Max.lua
@@ -1,18 +1,30 @@
 local placeId = game.PlaceId
 
 if placeId == 537413528 then
---Build A Boat For Treasure
-loadstring(game:HttpGet("https://raw.githubusercontent.com/buslate/Build-A-Boat-For-Treasure/main/Build%20A%20Boat%20V.2.lua"))()
-elseif placeId == 3351674303 then
+    BABFT = true
+end
 
+if BABFT then
+    repeat wait() until game:IsLoaded()
+	if getgenv().Setting then else
+        getgenv().Setting = {
+            ["Kaitun"] = false
+        }
+    end
+end
+if getgenv().Setting["Kaitun"] == true then
+     loadstring(game:HttpGet(("https://raw.githubusercontent.com/buslate/Kaitun-BAFT/main/kai/na/hee/kuy.lua"), true))()
+elseif getgenv().Setting["Kaitun"] == false then
+     loadstring(game:HttpGet(("https://raw.githubusercontent.com/buslate/Build-A-Boat-For-Treasure/main/Build%20A%20Boat%20V.2.lua"), true))()
+elseif placeId == 3351674303 then
 --Driving Empire
 loadstring(game:HttpGet("https://raw.githubusercontent.com/buslate/Driving-Empire/main/.Lua"))()
+
 elseif placeId == 11364184405 then
-
     --Super Fun Obby
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/buslate/Super-Fun-Obby-725-Stages-/main/README.md"))()
-elseif placeId == 2753915549 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/buslate/Super-Fun-Obby-725-Stages-/main/README.md"))()
 
+elseif placeId == 2753915549 then
 --BloxFruit
 loadstring(game:HttpGet("https://raw.githubusercontent.com/buslate/Blox-Fruit/main/.Lua"))()
 
@@ -22,11 +34,11 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/buslate/Clicker-Madne
 
 elseif placeId == 13364215404 then
      --Ezan Zone Boxing
-     loadstring(game:HttpGet("https://raw.githubusercontent.com/buslate/-Ezan-Zone-Boxing/main/.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/buslate/-Ezan-Zone-Boxing/main/.lua"))()
 
 elseif placeId == 6447798030 then
      --fnf
-     loadstring(game:HttpGet("https://raw.githubusercontent.com/buslate/fnf/main/.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/buslate/fnf/main/.lua"))()
 
 elseif placeId == 5210095481 then
 --omini x
